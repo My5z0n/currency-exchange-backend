9b8fdd626584044ca4fe8be3a64782b894cddc1f
Szymon Nagel <szmnnagel@gmail.com>
2023-04-23 21:51:35 +0200

    Added in exchante_rate tests for future and past time

--- app/tests/exchange_rate_tests.py
+++ app/tests/exchange_rate_tests.py
@@ -1,8 +1,10 @@
 from fastapi.testclient import TestClient
 from unittest.mock import MagicMock
+from datetime import date, timedelta
 from api.services.currency_service import CurrencyService
 from api.core.exceptions import NoDataError
 from main import app
+
 
 client = TestClient(app)
 
@@ -41,3 +43,60 @@ def test_get_exchange_rate_handler_invalid_date():
     # Verify the response is as expected
     assert response.status_code == 422
     assert response.json() == {"message": "Invalid Request"}
+
+
+def test_get_exchange_rate_handler_invalid_currency():
+    # Send a request to the API with an invalid currency code
+    response = client.get("/api/v1/exchanges/INVALID/2023-01-01")
+
+    # Verify the response is as expected
+    assert response.status_code == 422
+    assert response.json() == {"message": "Invalid Request"}
+
+
+def test_get_exchange_rate_handler_future_date():
+    # Send a request to the API with a future date
+    tomorrow = date.today() + timedelta(1)
+    response = client.get(
+        f"/api/v1/exchanges/USD/{tomorrow.strftime('%Y-%m-%d')}")
+
+    # Verify the response is as expected
+    assert response.status_code == 422
+    assert response.json() == {"message": "Invalid Request"}
+
+
+def test_get_exchange_rate_handler_past_date():
+    # Send a request to the API with a past date that is not available in the database
+    response = client.get("/api/v1/exchanges/USD/2019-01-01")
+
+    # Verify the response is as expected
+    assert response.status_code == 404
+    assert response.json() == {"message": "Data not found"}
+
+
+def test_get_exchange_rate_handler_connection_error():
+    # Mock the CurrencyService.get_exchange_rate method to raise a ConnectionError
+    mock_service = MagicMock(spec=CurrencyService)
+    mock_service.get_exchange_rate.side_effect = ConnectionError(
+        "Unable to connect to server")
+
+    # Send a request to the API
+    response = client.get("/api/v1/exchanges/USD/2023-01-01")
+
+    # Verify the response is as expected
+    assert response.status_code == 503
+    assert response.json() == {"message": "Service Unavailable"}
+
+
+def test_get_exchange_rate_handler_timeout():
+    # Mock the CurrencyService.get_exchange_rate method to raise a TimeoutError
+    mock_service = MagicMock(spec=CurrencyService)
+    mock_service.get_exchange_rate.side_effect = TimeoutError(
+        "Connection timed out")
+
+    # Send a request to the API
+    response = client.get("/api/v1/exchanges/USD/2023-01-01")
+
+    # Verify the response is as expected
+    assert response.status_code == 504
+    assert response.json() == {"message": "Gateway Timeout"}
